3f92a3b370fd59588caaa4e2806a51465f3084bb
rodolpholn <rodolpho-crack@hotmail.com>
2026-03-30 15:39:53 -0300

    fix: habilitando scalar em produção

--- api/EcoMonitor.Api/Program.cs
+++ api/EcoMonitor.Api/Program.cs
@@ -28,9 +28,9 @@ var app = builder.Build();
 
 
 // 2. Interface Visual do Scalar (O novo Swagger)
-
-    app.MapOpenApi();
-    app.MapScalarApiReference(); // Cria a página visual em /scalar/v1
+app.UseHttpsRedirection();
+app.MapOpenApi();
+app.MapScalarApiReference(); // Cria a página visual em /scalar/v1
 app.UseCors("PermitirAngular");
 app.UseHttpsRedirection();
 app.MapControllers();
